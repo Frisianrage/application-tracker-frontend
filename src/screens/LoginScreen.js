14f06578b00ba20dc981cc783fccf035cd864d28
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2023-03-03 12:51:12 -0800

    mobile responsiveness login register
    Update on the  mobile responsiveness for loginscreen and registerscreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,77 +1,87 @@
-import React, {useState, useEffect, useContext} from 'react'
-import { AuthContext } from '../context/auth'
-import {useNavigate, Link} from 'react-router-dom'
-import FormContainer from '../components/FormContainer'
-import {Form, Button, Container, Modal} from 'react-bootstrap'
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../context/auth';
+import { useNavigate, Link } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+import { Form, Button, Container, Modal } from 'react-bootstrap';
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [showModal, setShowModal] = useState(true)
-    
-    const history = useNavigate();
-    
-    const {user,login} = useContext(AuthContext)
-    
-    const submitHandler = (e) => {
-        e.preventDefault()
-        login(email, password) 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showModal, setShowModal] = useState(true);
+
+  const history = useNavigate();
+
+  const { user, login } = useContext(AuthContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  const onHide = () => {
+    setShowModal(false);
+    history('/');
+  };
+
+  useEffect(() => {
+    if (user) {
+      history('/dashboard');
     }
+  }, [user, history]);
 
-    const onHide = () => {
-        setShowModal(false)
-        history('/')
-    }
-
-    useEffect(() => {
-        if(user){
-            history('/dashboard')
-        }
-    }, [user,history])
-
-    return (
-        <Modal
-            show={showModal}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+  return (
+    <Modal
+      show={showModal}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title as="h1" id="contained-modal-title-vcenter">
+          Login
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <FormContainer>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Container
+              className="justify-content-center pt-3"
+              style={{ display: 'flex' }}
             >
-            <Modal.Header closeButton>
-            <Modal.Title as="h1" id="contained-modal-title-vcenter">
+              <Button type="submit" variant="primary" className="mx-1">
                 Login
-            </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <FormContainer>
-                    <Form onSubmit={submitHandler}>
-                        <Form.Group controlId='email'>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter your email address' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId='password'>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-                        </Form.Group>
-                        <Container className="justify-content-center pt-3">
-                            <Button type='submit' variant='primary' className="mx-2">
-                                Login
-                            </Button>
-                            <Button onClick={onHide} variant='primary'>
-                                Cancel
-                            </Button>  
-                        </Container>
-                    </Form> 
-                </FormContainer>
-            </Modal.Body>
-            <Modal.Footer className="justify-content-center">
-                No profile yet? To register click 
-                <Link to='/signin'>
-                    here...
-                </Link>     
-            </Modal.Footer>
-        </Modal>
-    )
-}
+              </Button>
+              <Button onClick={onHide} variant="primary" className="mx-1">
+                Cancel
+              </Button>
+            </Container>
+          </Form>
+        </FormContainer>
+      </Modal.Body>
+      <Modal.Footer className="justify-content-center">
+        No profile yet? To register click
+        <Link to="/signin">here...</Link>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 
-
-export default LoginScreen
+export default LoginScreen;
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,77 +1,87 @@
-import React, {useState, useEffect, useContext} from 'react'
-import { AuthContext } from '../context/auth'
-import {useNavigate, Link} from 'react-router-dom'
-import FormContainer from '../components/FormContainer'
-import {Form, Button, Container, Modal} from 'react-bootstrap'
-
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../context/auth';
+import { useNavigate, Link } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+import { Form, Button, Container, Modal } from 'react-bootstrap';
 
 function RegisterScreen() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [showModal, setShowModal] = useState(true)
-    
-    const history = useNavigate();
-    
-    const {user, register} = useContext(AuthContext)
-    
-    const submitHandler = async(e) => {
-        e.preventDefault()
-        register(email, password)
-    }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showModal, setShowModal] = useState(true);
 
-    const onHide = () => {
-        setShowModal(false)
-        history('/')
-    }
+  const history = useNavigate();
 
-    useEffect(() => {
-        if(user){
-            history('/dashboard')
-        }
-    }, [user,history])
+  const { user, register } = useContext(AuthContext);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    register(email, password);
+  };
+
+  const onHide = () => {
+    setShowModal(false);
+    history('/');
+  };
+
+  useEffect(() => {
+    if (user) {
+      history('/dashboard');
+    }
+  }, [user, history]);
 
   return (
     <Modal
-        show={showModal}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        >
-        <Modal.Header closeButton>
+      show={showModal}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
         <Modal.Title as="h1" id="contained-modal-title-vcenter">
-            Signin
+          Signin
         </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <FormContainer>
-                <Form onSubmit={submitHandler}>
-                    <Form.Group controlId='email'>
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control type='email' placeholder='Enter your email address' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-                    </Form.Group>
-                    <Container className="justify-content-center pt-3">
-                        <Button type='submit' variant='primary' className="mx-2">
-                            Signin
-                        </Button>
-                        <Button onClick={onHide} variant='primary'>
-                            Cancel
-                        </Button>  
-                    </Container>
-                </Form> 
-            </FormContainer>
-        </Modal.Body>
-        <Modal.Footer className="justify-content-center">
-            Already got a profile? To login click 
-            <Link to='/login'>
-                here...
-            </Link>  
-        </Modal.Footer>
+      </Modal.Header>
+      <Modal.Body>
+        <FormContainer>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Container
+              className="justify-content-center pt-3"
+              style={{ display: 'flex' }}
+            >
+              <Button type="submit" variant="primary" className="mx-1">
+                Signin
+              </Button>
+              <Button onClick={onHide} variant="primary" className="mx-1">
+                Cancel
+              </Button>
+            </Container>
+          </Form>
+        </FormContainer>
+      </Modal.Body>
+      <Modal.Footer className="justify-content-center">
+        Already got a profile? To login click
+        <Link to="/login">here...</Link>
+      </Modal.Footer>
     </Modal>
-);
+  );
 }
 
-export default RegisterScreen
+export default RegisterScreen;
