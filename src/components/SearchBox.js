df72e3a938734c0be086a75d74250e0d8aaaa648
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2023-03-03 17:43:34 -0800

    update responsiveness find employer component
    update the responsiveness of the find employer component incl. the searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,38 +1,53 @@
-import React, {useState} from 'react'
-import axios from 'axios'
-import {Form, Button} from 'react-bootstrap'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form, Button } from 'react-bootstrap';
 
-const SearchBox = ({setEmployerData}) => {
-    const [keyword, setKeyword] = useState('')
+const SearchBox = ({ setEmployerData }) => {
+  const [keyword, setKeyword] = useState('');
 
-    const token = localStorage.getItem('jwtToken')
+  const token = localStorage.getItem('jwtToken');
 
-    const config = {
-        baseURL: process.env.REACT_APP_BASEURL,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}` 
-        }
-      }
+  const config = {
+    baseURL: process.env.REACT_APP_BASEURL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    const clickHandler = async (e) => {
-        e.preventDefault()
-        if(keyword.trim()) {
-            const {data} = await axios.get(`/api/employers/search/${keyword}`, config)
-            setEmployerData(data.employers)
-        } else {
-            console.log("error")
-        }
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      const { data } = await axios.get(
+        `/api/employers/search/${keyword}`,
+        config
+      );
+      setEmployerData(data.employers);
+    } else {
+      console.log('error');
     }
+  };
 
-    return (
-        <Form >
-            <Form.Control type='text' name='q' onChange={(e) => setKeyword(e.target.value)} placeholder='Search Employer...' className='mr-sm-2 ml-sm-5'></Form.Control>
-            <Button type='submit' variant='primary' className='m-4' onClick={clickHandler}>
-                Search
-            </Button>
-        </Form>
-    )
-}
+  return (
+    <Form>
+      <Form.Control
+        type="text"
+        name="q"
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder="Search Employer..."
+        className="mr-sm-2 ml-sm-5"
+      ></Form.Control>
+      <Button
+        size="sm"
+        type="submit"
+        variant="primary"
+        className="m-4"
+        onClick={clickHandler}
+      >
+        Search
+      </Button>
+    </Form>
+  );
+};
 
-export default SearchBox
+export default SearchBox;
