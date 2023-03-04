524a2c53f7d6e77bf20fdb8a595430302589e685
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2023-03-03 16:21:12 -0800

    mobile responsiveness dashboard
    Update the mobile responsiveness of the dashboardscreen incl. all components

--- src/components/LastEmployer.js
+++ src/components/LastEmployer.js
@@ -1,79 +1,80 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import moment from 'moment'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Container, Table, Row, Col } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container, Table, Row, Col } from 'react-bootstrap';
 
 function LastEmployer() {
-  const [userData, setUserData] = useState('')
-  const [isLoading, setIsLoading] = useState('false')
+  const [userData, setUserData] = useState('');
+  const [isLoading, setIsLoading] = useState('false');
 
-  const token = localStorage.getItem('jwtToken')
-  
+  const token = localStorage.getItem('jwtToken');
+
   useEffect(() => {
     const getEmployer = async () => {
-      setIsLoading(true)
-      
+      setIsLoading(true);
+
       try {
-        const {data} = await axios.get('/api/employers/lastchanged', {
+        const { data } = await axios.get('/api/employers/lastchanged', {
           baseURL: process.env.REACT_APP_BASEURL,
           headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}` 
-          }
-        })
-       
-        if(data) {
-          setUserData(data)
-        } 
-      } catch (error) {
-        console.log(error)
-      }
-    }
-      getEmployer()
-      setIsLoading(false)
-  }, [isLoading, token])
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-    
+        if (data) {
+          setUserData(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getEmployer();
+    setIsLoading(false);
+  }, [isLoading, token]);
+
   return (
-      <Container>
-        <Row className="align-items-center">
-          <Col>
-              <h5><u>Last changed Employer</u></h5>
-          </Col>
-        </Row>
-          <Table striped bordered hover responsive className='table-sm'>
-            <thead>
+    <Container>
+      <Row className="align-items-center">
+        <Col>
+          <h5>
+            <u>Last changed Employer</u>
+          </h5>
+        </Col>
+      </Row>
+      <Table striped bordered hover responsive="sm" className="table-sm">
+        <thead>
+          <tr>
+            <th>COMPANY</th>
+            <th>LOCATION</th>
+            <th>DATE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData && userData[0] ? (
+            <LinkContainer to={`/employers/profile/${userData[0]._id}`}>
               <tr>
-                <th>COMPANY</th>
-                <th>CITY</th>
-                <th>STATE</th>
-                <th>COUNTRY</th>
-                <th>APPLICATIONS</th>
-                <th>CREATED AT</th>
+                <td>{userData[0].companyname}</td>
+                <td>
+                  {userData[0].location.city} / {userData[0].location.state} (
+                  {userData[0].location.country})
+                </td>
+
+                <td>{moment(userData[0].createdAt).format('MM/DD/YY')}</td>
               </tr>
-            </thead>
-            <tbody>
-                {userData && userData[0] ? (
-                    <LinkContainer to={`/employers/profile/${userData[0]._id}`}>
-                        <tr>
-                            <td>{userData[0].companyname}</td>
-                            <td>{userData[0].location.city}</td>
-                            <td>{userData[0].location.state}</td>
-                            <td>{userData[0].location.country}</td>
-                            <td>{userData[0].applications.length}</td>
-                            <td>{moment(userData[0].createdAt).format('MM/DD/YY')}</td>   
-                        </tr>
-                    </LinkContainer>
-                ) : (
-                <tr>
-                  <td>NO APPLICATIONS FOUND!</td>
-                </tr> 
-              )}
-            </tbody>
-          </Table>
-      </Container>
-  )
+            </LinkContainer>
+          ) : (
+            <tr>
+              <td>NO APPLICATIONS FOUND!</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
 
-export default LastEmployer
+export default LastEmployer;
+
+//<td>{userData[0].applications.length}</td>
--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,43 +1,64 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap';
 import MostRecentApp from '../components/MostRecentApp';
-import MostAppliedEmployer from '../components/MostAppliedEmployer'
-import LastApplication from '../components/LastApplication'
-import LastEmployer from '../components/LastEmployer'
+import MostAppliedEmployer from '../components/MostAppliedEmployer';
+import LastApplication from '../components/LastApplication';
+import LastEmployer from '../components/LastEmployer';
 
 function DashboardScreen() {
+  const history = useNavigate();
 
-  const history = useNavigate()
-
-  const token = localStorage.getItem('jwtToken')
-  if(!token) {
-      history('/')
+  const token = localStorage.getItem('jwtToken');
+  if (!token) {
+    history('/');
   }
 
   return (
     <Container>
       <Row className="align-items-center">
-          <Col className="my-3">
-              <h2><u>Dashboard</u></h2>
-          </Col>
-        </Row>
-      <Row className="m-1">
-        <Col className="m-2 p-2" style={{border: "solid lightgrey 1px", borderRadius: "10px"}}>
+        <Col className="my-3">
+          <h2>
+            <u>Dashboard</u>
+          </h2>
+        </Col>
+      </Row>
+      <Row className="p-2">
+        <Col
+          className="m-2"
+          style={{
+            border: 'solid lightgrey 1px',
+            borderRadius: '10px',
+            padding: '0.5rem 0.75rem',
+          }}
+        >
           <MostRecentApp />
         </Col>
-        <Col className="m-2 p-2" style={{border: "solid lightgrey 1px", borderRadius: "10px"}}>
+        <Col
+          className="m-2"
+          style={{
+            border: 'solid lightgrey 1px',
+            borderRadius: '10px',
+            padding: '0.5rem 0.75rem',
+          }}
+        >
           <MostAppliedEmployer />
         </Col>
       </Row>
-      <Row className="p-2 mx-3 my-5" style={{border: "solid lightgrey 1px", borderRadius: "10px"}}>
+      <Row
+        className="p-2 mx-1 my-5"
+        style={{ border: 'solid lightgrey 1px', borderRadius: '10px' }}
+      >
         <LastApplication />
       </Row>
-      <Row className="p-2 mx-3 my-5" style={{border: "solid lightgrey 1px", borderRadius: "10px"}}>
+      <Row
+        className="p-2 mx-1 my-5"
+        style={{ border: 'solid lightgrey 1px', borderRadius: '10px' }}
+      >
         <LastEmployer />
       </Row>
-    </Container>   
+    </Container>
   );
 }
 
-export default DashboardScreen
+export default DashboardScreen;
